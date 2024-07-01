7c9c6e4cf53767d3cfa0a359e1c571adbef694b6
Brandan D. <brandandelafuente@gmail.com>
2024-06-30 23:38:19 -0400

    add todo

--- fluff/cogs/basic.py
+++ fluff/cogs/basic.py
@@ -29,14 +29,14 @@ class Basic(Cog):
         self.bot = bot
         matplotlib.use("agg")
 
+
+    '''
+    TODO: stop this from being stupid
+    '''
     @commands.command()
     @commands.check(ismod)
     async def drive(self, ctx):
         """This spits out the Undertale Discord's Google Drive, but only if you're a mod."""
-        if not ctx.ismod:
-            return await ctx.reply(
-                content="You're not a mod! Stop bothering me."
-            )
         await ctx.send(
             f"https://drive.google.com/drive/folders/0B7weGRL72yVXUWJ3TEZ1NUd5QW8?usp=sharing"
         )
