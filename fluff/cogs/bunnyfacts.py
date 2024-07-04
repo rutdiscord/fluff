463350b87a0d138febb0e865c551ab3975d1d09d
thereal-marr <marrthefurby@gmail.com>
2024-07-04 16:23:15 -0400

    Update bunnyfacts.py

--- fluff/cogs/bunnyfacts.py
+++ fluff/cogs/bunnyfacts.py
@@ -5,8 +5,7 @@ from discord.ext.commands import Cog
 from helpers.embeds import stock_embed, author_embed, sympage
 from helpers.sv_config import get_config
 
-# this entire page is made wholesale by marr so it looks like absolute shit, if anybody wants to come and fix it and make it function like the help command 
-# where ppl can call individual commands linked to this feel free
+# this entire page is made wholesale by marr so it looks like absolute shit
 
 class BunnyFacts(Cog):
     """
@@ -19,6 +18,11 @@ class BunnyFacts(Cog):
             "binky": [
                 "https://media-be.chewy.com/wp-content/uploads/2022/05/24112223/rabbit-binkying.gif",
                 "https://global.discourse-cdn.com/business5/uploads/gemsofwar/original/3X/8/4/84364684586e83b84361ea90fce93dae8d0888d7.gif"
+                "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/93c11ea7-9de5-46e6-a1d7-b613cf57e399/d9753cz-e8e8a691-6bc2-4bfb-a9b3-af87a62809af.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzkzYzExZWE3LTlkZTUtNDZlNi1hMWQ3LWI2MTNjZjU3ZTM5OVwvZDk3NTNjei1lOGU4YTY5MS02YmMyLTRiZmItYTliMy1hZjg3YTYyODA5YWYuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0._B1a6DK7z1wDt50Ml0xWlyZN6BRDJBRyhviGmTHf4c8"
+                "https://elizabethannemartins.com/wp-content/uploads/2016/05/tumblr_mn7spsxk191ss93ulo1_400.gif?w=350&h=276"
+                "https://files.catbox.moe/m8us2w.gif"
+                "https://imgur.com/1fIf7o3"
+                "https://imgur.com/binky-rabbit-bunny-1fIf7o3"
             ]
         }
 
