915476ae9098241b7b8f0f023550f0b63836e064
thereal-marr <marrthefurby@gmail.com>
2024-07-04 15:05:48 -0400

    i have a type and its the worst type

--- fluff/cogs/bunnyfacts.py
+++ fluff/cogs/bunnyfacts.py
@@ -4,6 +4,9 @@ from discord.ext.commands import Cog
 from helpers.embeds import stock_embed, author_embed, sympage
 from helpers.sv_config import get_config
 
+# this entire page is made wholesale by marr so it looks like absolute shit, if anybody wants to come and fix it and make it function like the help command 
+# where ppl can call individual commands linked to this feel free
+
 class BunnyFacts(Cog):
     """
     Bunny facts!
@@ -13,14 +16,7 @@ class BunnyFacts(Cog):
         self.bot = bot
 
     @commands.command()
-    async def bunfact(self, ctx, *, fact=None):
-        """This is Fluff's bunny facts command.
-
-        Giving the fact name will show that fact's description.
-
-        - `fact name`
-        The fact to see. Optional."""
-        if not fact:
+    async def bunfact(self, ctx,):
             help_embed = stock_embed(self.bot)
             help_embed.set_author(name="Fluff", url="https://github.com/dfault-user/fluff", icon_url="https://cdn.discordapp.com/attachments/629713406651531284/1256428667345834014/3be16Ny.png?ex=668164a1&is=66801321&hm=d60b695a687388f6b7de1911b788676f12b56c630157e4a2c0249cc431faa5f6&")
             help_embed.add_field(name="Binky", value="What is a binky!?", inline=True)
@@ -33,12 +29,14 @@ class BunnyFacts(Cog):
             help_embed.add_field(name="Boxing & Lunging", value="Bunnies box and lunge at each other!?", inline=True)
             help_embed.add_field(name="Getting a Bunny", value="Rescuing a bunny...", inline=True)
             return await ctx.reply(embed=help_embed,mention_author=True)
-        else:
-            botcommand = self.bot.get_fact(fact)
-            if not botcommand:
-                return await ctx.reply(
-                    "This isn't a configured bunny fact.",
-                    mention_author=False,
-                )
+            
+    @commands.command()
+    @bunfact.command()
+    async def binky(self, ctx,):
+            help_embed = stock_embed(self.bot)
+            help_embed.set_author(name="Fluff", url="https://github.com/dfault-user/fluff", icon_url="https://cdn.discordapp.com/attachments/629713406651531284/1256428667345834014/3be16Ny.png?ex=668164a1&is=66801321&hm=d60b695a687388f6b7de1911b788676f12b56c630157e4a2c0249cc431faa5f6&")
+            help_embed.add_field(name="Binky", value="What is a binky!?", inline=True)
+            return await ctx.reply(embed=help_embed,mention_author=True)
+            
 async def setup(bot):
     await bot.add_cog(BunnyFacts(bot))
