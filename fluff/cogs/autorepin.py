755cc3f724da5b45cfe262d9423b705f2b1b051e
Brandan D. <brandandelafuente@gmail.com>
2024-07-05 12:30:00 -0400

    d'oh

--- fluff/cogs/autorepin.py
+++ fluff/cogs/autorepin.py
@@ -22,7 +22,7 @@ class Autorepin(commands.Cog):
             link_matches = {'guild': regex_match.group(1), # Guild
                         'channel': regex_match.group(2),  # Channel
                         'message': regex_match.group(3)} # Message
-            return await ctx.reply(f"Guild: {link_matches[0]}\nChannel:{link_matches[1]}\nMessage:{link_matches[2]}")
+            return await ctx.reply(f"Guild: {link_matches.guild}\nChannel:{link_matches.channel}\nMessage:{link_matches.message}")
         except (AttributeError, KeyError):
             return await ctx.reply(random_msg("err_generic") + ("(Regex failed to find a valid message link)"))
 
