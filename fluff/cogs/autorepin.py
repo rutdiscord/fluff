1726452fc395cdc61210d0184fc3f9d38b92bb19
Brandan D. <brandandelafuente@gmail.com>
2024-07-05 12:27:48 -0400

    add comments and fix something Lol

--- fluff/cogs/autorepin.py
+++ fluff/cogs/autorepin.py
@@ -13,14 +13,15 @@ class Autorepin(commands.Cog):
     @commands.guild_only()
     @commands.command()
     async def repin(self, ctx, msglink):
+        # Setup regular expression
         message_link_regex = r"\/([0-9].*)\/([0-9].*)\/([0-9].*[^/])\/{0,}"
         regex_match = re.search(message_link_regex, msglink)
         link_matches = {}
 
         try:
-            link_matches = {'guild': regex_match.group(1), 
-                        'channel': regex_match.group(2), 
-                        'message': regex_match.group(3)}
+            link_matches = {'guild': regex_match.group(1), # Guild
+                        'channel': regex_match.group(2),  # Channel
+                        'message': regex_match.group(3)} # Message
             return await ctx.reply(f"Guild: {link_matches[0]}\nChannel:{link_matches[1]}\nMessage:{link_matches[2]}")
         except AttributeError:
             return await ctx.reply(random_msg("err_generic") + ("(Regex failed to find a valid message link)"))
