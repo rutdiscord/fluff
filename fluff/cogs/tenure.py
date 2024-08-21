165f5eee0acd5a4cdcab04193566ad9c576de0ec
brandan d. <brandandelafuente@gmail.com>
2024-08-21 00:41:30 -0400

    ugh

--- fluff/cogs/tenure.py
+++ fluff/cogs/tenure.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext.commands import Cog
 from discord.ext import commands, tasks
 from helpers.sv_config import get_config
-from datetime import datetime, timezone
+from datetime import datetime, UTC
 class Tenure(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -22,7 +22,7 @@ class Tenure(commands.Cog):
                 guild, get_config(guild.id, "logging", "modlog")
                 )
         
-        return await modlog_channel.send(f"♾ **{member.global_name}** (**{member.id}**) has been in this server since {datetime.now(tz='UTC') - member.joined_at}")
+        return await modlog_channel.send(f"♾ **{member.global_name}** (**{member.id}**) has been in this server since {datetime.now(datetime.UTC) - member.joined_at}")
         
 
 async def setup(bot):
