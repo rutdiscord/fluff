4bb5e4220ade494efdaea98b5035daa2abdbed86
thereal-marr <marrthefurby@gmail.com>
2024-08-23 21:41:09 -0400

    binky update

--- fluff/cogs/bunnyfacts.py
+++ fluff/cogs/bunnyfacts.py
@@ -17,14 +17,14 @@ class BunnyFacts(Cog):
 
         self.facts = {
             "binky": {
-            "summary": "What is a binky!?",
+            "summary": "'An inexplicable expression of joy...'",
             "images": {"https://media-be.chewy.com/wp-content/uploads/2022/05/24112223/rabbit-binkying.gif",
                 "https://global.discourse-cdn.com/business5/uploads/gemsofwar/original/3X/8/4/84364684586e83b84361ea90fce93dae8d0888d7.gif",
                 "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/93c11ea7-9de5-46e6-a1d7-b613cf57e399/d9753cz-e8e8a691-6bc2-4bfb-a9b3-af87a62809af.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzkzYzExZWE3LTlkZTUtNDZlNi1hMWQ3LWI2MTNjZjU3ZTM5OVwvZDk3NTNjei1lOGU4YTY5MS02YmMyLTRiZmItYTliMy1hZjg3YTYyODA5YWYuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0._B1a6DK7z1wDt50Ml0xWlyZN6BRDJBRyhviGmTHf4c8",
                 "https://elizabethannemartins.com/wp-content/uploads/2016/05/tumblr_mn7spsxk191ss93ulo1_400.gif?w=350&h=276",
                 "https://files.catbox.moe/m8us2w.gif",
                 "https://files.catbox.moe/p2fg1y.gif"},
-            "fact": "When a rabbit does a weird twist and jump in the air, it\'s called a binky. Rabbits do this when they have so much happy energy that they just can\'t contain it. A binkying bunny is a very happy bunny!\n\n**Why do rabbits binky?**\n We don\'t really know why rabbits binky. All we know is that it\'s a normal instinct in rabbits. It\'s like an inexplicable expression of joy, much like laughing is for humans.\n[Link](https://bunnylady.com/bunny-binkies/)"
+            "fact": "A funky little jump along with a flick of the back legs (or head, if it\'s a smaller binky!). It\'s a way rabbits express excess energy they can\'t contain, usually joy. A binkying bunny is a very happy bunny!\n\n**Why do rabbits binky?**\n Nobody really knows why rabbits binky. All we know is that it\'s an instinct, an inexplicable expression of joy, much like laughing is for humans.\nBaby rabbits binky more often than adult rabbits - they\'re more energetic while adults are generally sleepier and calmer. Like puppies!\n[Link](https://bunnylady.com/bunny-binkies/)"
             },
             "flopping": {
             "summary": "Is that bunny dead? (no)",
