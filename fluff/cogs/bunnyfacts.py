1c64c875955328e6f10d0306c12a7d1c5033166b
thereal-marr <marrthefurby@gmail.com>
2024-08-23 21:49:36 -0400

    h

--- fluff/cogs/bunnyfacts.py
+++ fluff/cogs/bunnyfacts.py
@@ -75,7 +75,7 @@ class BunnyFacts(Cog):
             }
         }
 
-    @commands.group(invoke_without_command=True, aliases=["bunfacts"])
+    @commands.group(invoke_without_command=True, aliases=["bunfacts, bunnyfacts"])
     async def bunfact(self, ctx: commands.Context):
             help_embed = stock_embed(self.bot)
             help_embed.set_author(name="Fluff", url="https://github.com/dfault-user/fluff", icon_url="https://cdn.discordapp.com/attachments/629713406651531284/1256428667345834014/3be16Ny.png?ex=668164a1&is=66801321&hm=d60b695a687388f6b7de1911b788676f12b56c630157e4a2c0249cc431faa5f6&")
