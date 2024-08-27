0f5b5007091a8001809f03c205f28b4ddeaaa067
brandan d. <brandandelafuente@gmail.com>
2024-08-27 19:49:17 -0400

    fuck

--- fluff/cogs/tenure.py
+++ fluff/cogs/tenure.py
@@ -1,5 +1,6 @@
 import discord
 import asyncio
+import json
 from discord.ext.commands import Cog
 from discord.ext import commands
 from helpers.sv_config import get_config
@@ -102,10 +103,10 @@ class Tenure(Cog):
             return
         
         tenure_user = get_userfile(msg.author.id, "tenure")
-        
+
         if "bl" not in tenure_user:
             tenure_user["bl"] = False
-            set_userfile(msg.author.id, "tenure", tenure_user)
+            set_userfile(msg.author.id, "tenure", json.dumps(tenure_user))
             
         tenureconfig = self.get_tenureconfig(msg.guild)
         tenure_dt = await self.check_joindelta(msg.author)
