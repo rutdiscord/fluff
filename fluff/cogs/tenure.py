27e14553211a98dffce09ffc69f2c8a4788b6a2e
brandan d. <brandandelafuente@gmail.com>
2024-08-27 20:36:24 -0400

    p

--- fluff/cogs/tenure.py
+++ fluff/cogs/tenure.py
@@ -141,7 +141,7 @@ class Tenure(Cog):
             if user.id in tenure_bl:
                 tenure_bl.remove(str(user.id))
         
-        set_guildfile(ctx.guild.id, "tenure", {"bl": tenure_bl})
+        set_guildfile(ctx.guild.id, "tenure", json.dumps({"bl": tenure_bl}))
         await ctx.reply("Users whitelisted for being tenured.", mention_author=False)
 
 
