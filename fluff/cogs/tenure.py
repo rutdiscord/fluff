16c483dcc3b4d94cb4d581bd962327ce79765dab
brandan d. <brandandelafuente@gmail.com>
2024-09-03 09:10:41 -0400

    dead code LOL

--- fluff/cogs/tenure.py
+++ fluff/cogs/tenure.py
@@ -3,7 +3,6 @@ import json
 from discord.ext.commands import Cog
 from discord.ext import commands
 from helpers.sv_config import get_config
-from helpers.datafiles import get_guildfile, set_guildfile
 from helpers.checks import ismanager, isadmin
 from datetime import datetime, timedelta, UTC
 from config import logchannel
