f1a25db67b98d898b820ebeb7b3f144751bdbe3c
brandan d. <brandandelafuente@gmail.com>
2024-09-06 13:44:39 -0400

    aaghh

--- fluff/cogs/rulepush_a2.py
+++ fluff/cogs/rulepush_a2.py
@@ -204,9 +204,10 @@ class RulePushV2(Cog):
 
                     if session["session_data"]["roles"]:
                         for role in session["session_data"]["roles"]:
-                            print(role)
-                            if role is not guild.default_role:
-                                await user.add_roles(guild.get_role(role))
+                            if guild.get_role(role) is not guild.default_role:
+                                await user.add_roles(
+                                    guild.get_role(role)
+                                )  # two api calls for the price of one
 
                     await channel.delete()
                     del rulepush_sessions["pushed"][session["channel"]]
