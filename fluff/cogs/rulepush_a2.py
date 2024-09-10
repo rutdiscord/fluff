f24ebddbd6a184ccbc5b8987fd24ada5f9011671
brandan d. <brandandelafuente@gmail.com>
2024-09-10 11:24:05 -0400

    Add local unfinished cogs to gitignore, put rulepush and rulepush (a2) away for now..

--- fluff/cogs/rulepush_a2.py
+++ fluff/cogs/rulepush_a2.py
@@ -10,30 +10,6 @@ from helpers.checks import ismod, ismanager
 from helpers.sv_config import get_config
 from helpers.datafiles import get_tossfile, set_tossfile
 
-"""
-    JSON format for rulepushes:
-    {
-        "pushed": {
-            "channel_name": {
-                "user_id": [
-                    "roles": [role_id, role_id, ...], 
-                    timestamp: 0
-                ]
-            },
-        },
-
-        "idle_kicked": {
-            "user_id": [
-            "roles": [role_id, role_id, ...], 
-            ],
-        },
-
-        "left": []
-
-        },
-    }
-"""
-
 
 class RulePushV2(Cog):
 
@@ -52,241 +28,94 @@ class RulePushV2(Cog):
             )
         )
 
-    async def session_manager(
-        self,
-        action: str,
-        guild: discord.Guild,
-        user: discord.Member = None,
-        channel: discord.TextChannel = None,
-    ):  # this is mostly to provide a predictable path to frequently used functions
+    def get_session(self, member):
+        tosses = get_tossfile(member.guild.id, "tosses")
+        if not tosses:
+            return None
+        session = None
+        if "LEFTGUILD" in tosses and str(member.id) in tosses["LEFTGUILD"]:
+            session = False
+        for channel in tosses:
+            if channel == "LEFTGUILD":
+                continue
+            if str(member.id) in tosses[channel]["tossed"]:
+                session = channel
+                break
+        return session
 
-        if action not in ["get", "create", "clean_destroy", "all_sessions"]:
-            raise NotImplementedError(
-                "Action not implemented"
-            )  # if action not supported, bail
+    async def new_session(self, guild):
+        staff_roles = [
+            self.bot.pull_role(guild, get_config(guild.id, "staff", "modrole")),
+            self.bot.pull_role(guild, get_config(guild.id, "staff", "adminrole")),
+        ]
+        bot_role = self.bot.pull_role(guild, get_config(guild.id, "staff", "botrole"))
+        tosses = get_tossfile(guild.id, "tosses")
 
-        if not self.enabled(guild):
-            return
+        for c in get_config(guild.id, "toss", "tosschannels"):
+            if c not in [g.name for g in guild.channels]:
+                if c not in tosses:
+                    tosses[c] = {"tossed": {}, "untossed": [], "left": []}
+                    set_tossfile(guild.id, "tosses", json.dumps(tosses))
 
-        rulepush_sessions = get_tossfile(guild.id, "rulepush")  # Pull tosses
-        if rulepush_sessions == {}:
-            rulepush_sessions = {
-                "pushed": {},
-                "idle_kicked": {},
-                "left": [],
-            }  # If no sessions, create empty dict
-
-        match action:
-            case "all_sessions":
-                if not rulepush_sessions:
-                    return
-                else:
-                    return rulepush_sessions
-            case "get":
-                if not rulepush_sessions:  # Prevention of Chaos
-                    return None
-
-                session = None
-
-                if (
-                    "left" in rulepush_sessions
-                    and str(user.id) in rulepush_sessions["left"]
-                ):
-                    session = False  # User left
-
-                for channel in rulepush_sessions["pushed"]:
-                    if str(user.id) in rulepush_sessions["pushed"][channel]:
-                        session = {
-                            "user_id": str(user.id),
-                            "channel": channel,
-                            "session_data": rulepush_sessions["pushed"][channel][
-                                str(user.id)
-                            ],
-                        }  # Found session
-                        break
-
-                return session
-            case "create":
-
-                user_current_session = await self.session_manager("get", guild, user)
-
-                rulepush_config_role = self.bot.pull_role(
-                    guild, get_config(guild.id, "rulepush", "rulepushrole")
-                )
-
-                rulepush_config_category = self.bot.pull_category(
-                    guild, get_config(guild.id, "rulepush", "rulepushcategory")
-                )
-
-                rulepush_config_topic = get_config(
-                    guild.id, "rulepush", "rulepushtopic"
-                )
-
-                if user_current_session:
-                    return user_current_session
-
-                staff_roles = [
-                    self.bot.pull_role(guild, get_config(guild.id, "staff", "modrole")),
-                    self.bot.pull_role(
-                        guild, get_config(guild.id, "staff", "adminrole")
+                overwrites = {
+                    guild.default_role: discord.PermissionOverwrite(
+                        read_messages=False
                     ),
-                ]
-
-                bot_role = self.bot.pull_role(
-                    guild, get_config(guild.id, "staff", "botrole")
-                )
-
-                for channel in get_config(guild.id, "rulepush", "rulepushchannels"):
-                    if channel not in [iterc.name for iterc in guild.channels]:
-                        try:
-                            if str(user.id) in rulepush_sessions["pushed"][channel]:
-                                return False
-                        except KeyError:
-                            pass
-
-                        if channel not in rulepush_sessions:
-                            rulepush_sessions["pushed"][channel] = {
-                                f"{user.id}": {
-                                    "roles": [role.id for role in user.roles],
-                                    "timestamp": int(
-                                        datetime.datetime.now().timestamp()
-                                    ),
-                                }
-                            }
-                            set_tossfile(
-                                guild.id, "rulepush", json.dumps(rulepush_sessions)
-                            )
-                            overwrites = {
-                                guild.default_role: discord.PermissionOverwrite(
-                                    read_messages=False
-                                ),
-                                guild.me: discord.PermissionOverwrite(
-                                    read_messages=True
-                                ),
-                            }
-                            if bot_role:
-                                overwrites[bot_role] = discord.PermissionOverwrite(
-                                    read_messages=True
-                                )
-                            for staff_role in staff_roles:
-                                if not staff_role:
-                                    continue
-                                overwrites[staff_role] = discord.PermissionOverwrite(
-                                    read_messages=True
-                                )
-
-                            rulepush_channel = await guild.create_text_channel(
-                                channel,
-                                reason="Fluff Rulepush",
-                                category=rulepush_config_category,
-                                overwrites=overwrites,
-                                topic=rulepush_config_topic,
-                            )
-
-                            await rulepush_channel.set_permissions(
-                                user, read_messages=True
-                            )
-
-                            await user.add_roles(rulepush_config_role)
-
-                            for role in user.roles:
-                                if role != rulepush_config_role:
-                                    if role.is_assignable():
-                                        await user.remove_roles(
-                                            role,
-                                            reason="Generating rulepush session (Fluff)",
-                                            atomic=False,
-                                        )
-                                    else:
-                                        return
-
-                            return rulepush_channel
-
-            case "clean_destroy":
-                rulepush_config_category = self.bot.pull_category(
-                    guild, get_config(guild.id, "rulepush", "rulepushcategory")
-                )
-
-                session = await self.session_manager("get", guild, user)
-
-                if session:
-                    await user.remove_roles(
-                        rulepush_config_role,
-                        reason="Dismantling rulepush session (Fluff)",
+                    guild.me: discord.PermissionOverwrite(read_messages=True),
+                }
+                if bot_role:
+                    overwrites[bot_role] = discord.PermissionOverwrite(
+                        read_messages=True
                     )
+                for staff_role in staff_roles:
+                    if not staff_role:
+                        continue
+                    overwrites[staff_role] = discord.PermissionOverwrite(
+                        read_messages=True
+                    )
+                toss_channel = await guild.create_text_channel(
+                    c,
+                    reason="Fluff Toss",
+                    category=self.bot.pull_category(
+                        guild, get_config(guild.id, "toss", "tosscategory")
+                    ),
+                    overwrites=overwrites,
+                    topic=get_config(guild.id, "toss", "tosstopic"),
+                )
 
-                    for role in session["session_data"]["roles"]:
-                        if guild.get_role(role) is not guild.default_role:
-                            await user.add_roles(
-                                guild.get_role(role), atomic=False
-                            )  # two api calls for the price of one
+                return toss_channel
 
-                    await channel.delete()
-                    del rulepush_sessions["pushed"][session["channel"]]
-                    set_tossfile(guild.id, "rulepush", json.dumps(rulepush_sessions))
-                    return True
-                else:
-                    return False
+    async def start_managed_session(self, user, staff, push_channel):
+        push_role = self.bot.pull_role(
+            user.guild, get_config(user.guild.id, "rulepush", "rulepushrole")
+        )
 
-    async def setup_session(session: discord.channel.TextChannel, user: discord.Member):
-        pass
+        if push_role in user.roles:
+            return False
 
-    @commands.bot_has_permissions(manage_roles=True, manage_channels=True)
-    @commands.group(invoke_without_command=True)
-    async def session_debug(self, ctx: commands.Context):
-        sessions = await self.session_manager("all_sessions", ctx.guild)
-        await ctx.reply(f"```\n{sessions}```", mention_author=False)
+        roles = []
+        for rx in user.roles:
+            if rx != user.guild.default_role and rx != push_role:
+                roles.append(rx)
 
-    @session_debug.command()
-    async def get(self, ctx: commands.Context, user: discord.Member):
-        session = await self.session_manager("get", ctx.guild, user)
-        await ctx.reply(f"```\n{session}```", mention_author=False)
+        pushes = get_tossfile(user.guild.id, "rulepushes")
+        pushes[push_channel.name]["pushed"][str(user.id)] = [role.id for role in roles]
+        set_tossfile(user.guild.id, "rulepushes", json.dumps(pushes))
 
-    @session_debug.command()
-    async def create(self, ctx: commands.Context, user: discord.Member):
-        session = await self.session_manager("create", ctx.guild, user)
-        await ctx.reply(f"```\n{session}```", mention_author=False)
-
-    @session_debug.command()
-    async def destroy(
-        self,
-        ctx: commands.Context,
-        us: discord.Member,
-        chan: discord.TextChannel,
-    ):
-        session = await self.session_manager("clean_destroy", ctx.guild, us, chan)
-        await ctx.reply(f"```\n{session}```", mention_author=False)
-
-    @commands.bot_has_permissions(manage_roles=True)
-    @commands.guild_only()
-    @commands.check(ismod)
-    @commands.command()
-    async def rulepush(self, ctx, user: discord.Member):
-        if not self.enabled(ctx.guild):
-            return
-
-        potential_session = await self.session_manager("get", ctx.guild, user)
-
-        if (potential_session) is not None:
-            return await ctx.reply(
-                f"User already has a session... *thump*", mention_author=False
+        await user.add_roles(push_role, reason="Fluff Rulepush")
+        fail_roles = []
+        if roles:
+            for rr in roles:
+                if not rr.is_assignable():
+                    fail_roles.append(rr)
+                    roles.remove(rr)
+            await user.remove_roles(
+                *roles,
+                reason=f"User rulepushed by {staff} ({staff.id})",
+                atomic=False,
             )
 
-        new_session = await self.session_manager("create", ctx.guild, user)
-
-    @Cog.listener()
-    async def on_message(self, message: discord.Message):
-        await self.bot.wait_until_ready()
-        if message.author.bot or message.is_system() or not message.guild:
-            return
-
-        rulepush_channels = get_config(message.guild.id, "rulepush", "rulepushchannels")
-
-        if message.author.bot:
-            return
-
-        if str(message.channel.id) not in rulepush_channels:
-            return
+        return fail_roles, roles
 
 
 async def setup(bot):
