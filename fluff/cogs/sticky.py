5cba3005c7bbb91037edfec2c3545d799ef3d4d7
thereal-marr <marrthefurby@gmail.com>
2025-03-27 22:01:15 -0400

    erewrewrwerew

--- fluff/cogs/sticky.py
+++ fluff/cogs/sticky.py
@@ -32,23 +32,6 @@ class StickyMessage(commands.Cog):
             return
 
         async def repost_task():
-            # Post the sticky message immediately when the task starts
-            if channel.id in self.sticky_messages:
-                last_message_id = self.sticky_messages[channel.id]["last_message_id"]
-
-                # Delete the previous sticky message if it exists
-                if last_message_id:
-                    try:
-                        last_message = await channel.fetch_message(last_message_id)
-                        await last_message.delete()
-                    except discord.NotFound:
-                        pass  # Message was already deleted
-
-                # Send the new sticky message
-                new_message = await channel.send(self.sticky_messages[channel.id]["message"])
-                self.sticky_messages[channel.id]["last_message_id"] = new_message.id
-                self.save_sticky_data()  # Save updated last_message_id
-
             while channel.id in self.sticky_messages:
                 # Wait for the interval
                 await asyncio.sleep(self.sticky_messages[channel.id]["interval"] * 60)
@@ -83,7 +66,6 @@ class StickyMessage(commands.Cog):
                 self.sticky_messages[channel.id]["last_message_id"] = new_message.id
                 self.save_sticky_data()  # Save updated last_message_id
 
-        # Start the reposting task
         self.repost_tasks[channel.id] = self.bot.loop.create_task(repost_task())
 
     def stopsticky(self, channel):
@@ -171,7 +153,12 @@ class StickyMessage(commands.Cog):
     async def on_message(self, message):
         """Reset the repost timer if a new message is sent in a sticky channel."""
         if message.channel.id in self.sticky_messages and not message.author.bot:
-            self.stopsticky(message.channel)
+            # Cancel the current task
+            if message.channel.id in self.repost_tasks:
+                self.repost_tasks[message.channel.id].cancel()
+
+            # Restart the sticky task with a delay
+            await asyncio.sleep(60)  # Wait 60 seconds before restarting the timer
             self.startsticky(message.channel)
 
 async def setup(bot):
