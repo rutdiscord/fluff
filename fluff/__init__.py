81c7390c18fc291276cb8b5e1737f6a4b32842b6
Meagus <47562734+meagus4@users.noreply.github.com>
2025-07-05 04:22:05 +1000

    Happy Birthday Atora (No, nothing uses presences)

--- fluff/__init__.py
+++ fluff/__init__.py
@@ -56,6 +56,7 @@ def get_prefix(bot, message):
 # Bot setup.
 intents = discord.Intents.all()
 intents.typing = False
+intents.presences = False
 
 bot = commands.Bot(
     command_prefix=get_prefix,
