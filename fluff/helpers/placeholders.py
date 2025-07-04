795beb03ab21badc5da6626fc4b4cb13ba991cf0
Meagus <47562734+meagus4@users.noreply.github.com>
2025-07-05 04:04:24 +1000

    placeholders: log_embed (dead), generate_rule(obsolete)

--- fluff/helpers/placeholders.py
+++ fluff/helpers/placeholders.py
@@ -18,29 +18,3 @@ def random_msg(variant, **fills):
                 continue
             string = string.replace("{" + name + "}", fills[name])
     return string.format(**shorthands)
-
-
-def generate_rule(rule, **fills):
-    rules = placeholders["rules"]
-    selected_rule = random.choice(rules[rule])
-    if fills:
-        for name in fills.keys():
-            if not "{" + name + "}" in selected_rule:
-                continue
-            selected_rule = selected_rule.replace("{" + name + "}", fills[name])
-    return selected_rule.format(**rules)
-
-
-def create_log_embed(bot, color, title, desc, author, fields, thumbnail=None):
-    embed = discord.Embed(
-        color=color,
-        title=title,
-        description=desc,
-        timestamp=datetime.datetime.now(),
-    )
-    embed.set_footer(text=bot.user.name, icon_url=bot.user.display_avatar)
-    embed.set_author(
-        name=str(author),
-        icon_url=author.display_avatar.url,
-    )
-    return embed
